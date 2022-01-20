695dbe90da7391259731486b027bb77041ff715a
Jan Heinrich Reimer <heinrich@reimer.family>
2022-01-21 00:48:13 +0100

    Add feature flags

--- chatnoir_pyterrier/retrieve.py
+++ chatnoir_pyterrier/retrieve.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass, field
+from enum import unique, auto, Flag
+from functools import reduce
 from itertools import islice
 from typing import Set, Optional, Iterable, Union, Any, Dict
 
@@ -12,6 +14,32 @@ from pandas.core.groupby import DataFrameGroupBy
 from pyterrier.batchretrieve import BatchRetrieveBase
 from pyterrier.model import add_ranks
 from tqdm import tqdm
+
+
+@unique
+class Feature(Flag):
+    NONE = 0
+    UUID = auto()
+    INDEX = auto()
+    TARGET_HOSTNAME = auto()
+    TARGET_URI = auto()
+    TARGET = TARGET_HOSTNAME | TARGET_URI
+    PAGE_RANK = auto()
+    SPAM_RANK = auto()
+    RANKS = PAGE_RANK | SPAM_RANK
+    TITLE_HIGHLIGHTED = auto()
+    TITLE_TEXT = auto()
+    TITLE = TITLE_HIGHLIGHTED | TITLE_TEXT
+    SNIPPET_HIGHLIGHTED = auto()
+    SNIPPET_TEXT = auto()
+    SNIPPET = SNIPPET_HIGHLIGHTED | SNIPPET_TEXT
+    EXPLANATION = auto()
+    HTML = auto()
+    HTML_PLAIN = auto()
+    ALL = (
+            UUID | INDEX | TARGET | RANKS | TITLE | SNIPPET | EXPLANATION |
+            HTML | HTML_PLAIN
+    )
 
 
 @dataclass
