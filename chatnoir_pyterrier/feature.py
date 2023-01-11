59d2c6ce5fa8cd039cf222b169f7f19fceb03006
Jan Heinrich Reimer <jan.reimer@student.uni-halle.de>
2023-01-11 23:24:41 +0100

    Fix features

--- chatnoir_pyterrier/feature.py
+++ chatnoir_pyterrier/feature.py
@@ -1,4 +1,10 @@
 from enum import unique, auto, Flag
+from functools import reduce
+from operator import or_
+
+
+def _combine_flags(*flags):
+    return reduce(or_, flags)
 
 
 @unique
@@ -7,31 +13,31 @@ class Feature(Flag):
     UUID = auto()
     TREC_ID = auto()
     WARC_ID = auto()
-    IDS = UUID | TREC_ID | WARC_ID
+    IDS = _combine_flags(UUID, TREC_ID, WARC_ID)
     INDEX = auto()
     CRAWL_DATE = auto()
     TARGET_HOSTNAME = auto()
     TARGET_URI = auto()
-    TARGET = TARGET_HOSTNAME | TARGET_URI
+    TARGET = _combine_flags(TARGET_HOSTNAME, TARGET_URI)
     CACHE_URI = auto()
     PAGE_RANK = auto()
     SPAM_RANK = auto()
-    RANKS = PAGE_RANK | SPAM_RANK
+    RANKS = _combine_flags(PAGE_RANK, SPAM_RANK)
     TITLE_HIGHLIGHTED = auto()
     TITLE_TEXT = auto()
-    TITLE = TITLE_HIGHLIGHTED | TITLE_TEXT
+    TITLE = _combine_flags(TITLE_HIGHLIGHTED, TITLE_TEXT)
     SNIPPET_HIGHLIGHTED = auto()
     SNIPPET_TEXT = auto()
-    SNIPPET = SNIPPET_HIGHLIGHTED | SNIPPET_TEXT
+    SNIPPET = _combine_flags(SNIPPET_HIGHLIGHTED, SNIPPET_TEXT)
     EXPLANATION = auto()
     CONTENT = auto()
     CONTENT_PLAIN = auto()
     CONTENT_TYPE = auto()
     LANGUAGE = auto()
-    ALL = (
-            UUID | TREC_ID | INDEX | TARGET | RANKS | TITLE | SNIPPET |
-            EXPLANATION | CONTENT | CONTENT_PLAIN
+    ALL = _combine_flags(
+        UUID, TREC_ID, INDEX, TARGET, RANKS, TITLE, SNIPPET, EXPLANATION,
+        CONTENT, CONTENT_PLAIN
     )
-    ALL_STAGING = (
-            ALL | WARC_ID | CRAWL_DATE | CACHE_URI | CONTENT_TYPE | LANGUAGE
+    ALL_STAGING = _combine_flags(
+        ALL, WARC_ID, CRAWL_DATE, CACHE_URI, CONTENT_TYPE, LANGUAGE
     )
